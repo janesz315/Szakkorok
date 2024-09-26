4e88c65c4a8eac3cb94f1f7258acedfaa11f3d43
janesz315 <borosuveg6@gmail.com>
2024-09-26 12:22:37 +0200

    Kártya elkészítése (kezdetleges)

--- src/components/SzakkorKartya.vue
+++ src/components/SzakkorKartya.vue
@@ -1,5 +1,11 @@
 <template>
-  
+  <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">{{  }}</p>
+    
+  </div>
+</div>
 </template>
 
 <script>
