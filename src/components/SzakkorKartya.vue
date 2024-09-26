e856779a36d6c7bcc4b659c9cbdaefc410c8f0ac
janesz315 <borosuveg6@gmail.com>
2024-09-26 20:43:46 +0200

    kártyák és a szakkörök működnek

--- src/components/SzakkorKartya.vue
+++ src/components/SzakkorKartya.vue
@@ -1,19 +1,37 @@
+
+
 <template>
-  <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">{{  }}</p>
-    
+  <div>
+    <div v-for="szakkor in szakkorok" :key="szakkor.id" class="card" style="width: auto; margin-bottom: 20px;">
+      <div class="card-body">
+        <h5 class="card-title">{{ szakkor.nev }}</h5>
+        <p class="card-text">
+          <span v-if="filteredTanulok(szakkor.id).length === 0">Nincs tanuló ebben a szakkörben</span>
+          <ul v-else>
+            <li v-for="tanulo in filteredTanulok(szakkor.id)" :key="tanulo.id">{{ tanulo.nev }}</li>
+          </ul>
+        </p>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    szakkorok: Array,
+    tanulok: Array,
+  },
+  methods: {
+    filteredTanulok(szakkorId) {
+      return this.tanulok.filter((tanulo) => tanulo.szakkorId === szakkorId);
+    },
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.card {
+  border: 2px solid green;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <h1 class="text-center">Szakkörök</h1>
-
+    
   <div class="container container-fluid my-border">
-    <h1>Am I too cute to handle?</h1>
+     
+  <div class="row">
     <div class="col col-6">
       <table class="table table-bordered border-success table-hover table-striped table-success">
         <thead>
@@ -20,148 +21,65 @@
               <select
                 class="form-select form-select-sm"
                 aria-label="Small select example"
+                v-model="tanulo.szakkorId"
               >
-                <option selected>Szakkörök listája</option>
-                <option v-for="(szakkor, ind) in szakkorok" :key="ind">
+                <option disabled value="">Válassz szakkört</option>
+                <option v-for="(szakkor, ind) in szakkorok" :key="ind" :value="szakkor.id">
                   {{ szakkor.nev }}
                 </option>
               </select>
             </td>
-            <!-- v-for="(szakkor, ind) in szakkorok" :key="ind" -->
           </tr>
         </tbody>
       </table>
     </div>
 
-    <div class="col col-6"></div>
+    <div class="col col-6 mt-2">
+      <SzakkorKartya :szakkorok="szakkorok" :tanulok="tanulok" />
+    </div>
+  </div>
   </div>
 </template>
+
 <script>
+import SzakkorKartya from '@/components/SzakkorKartya.vue';
+
 export default {
+  components: {
+    SzakkorKartya,
+  },
   data() {
     return {
       tanulok: [
-        {
-          id: 1,
-          nev: "AVE",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 2,
-          nev: "Lexus_DC",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 3,
-          nev: "Farlix",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 4,
-          nev: "Amy",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 5,
-          nev: "ItzKat1",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 6,
-          nev: "surix",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 7,
-          nev: "Begi",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 8,
-          nev: "SanielShadowX",
-          osztaly: "13a",
-          szakkorId: null,
-        },
-        {
-          id: 9,
-          nev: "Aaron Collins",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 10,
-          nev: "Patr1ck01",
-          osztaly: "14d",
-          szakkorId: null,
-        },
-        {
-          id: 11,
-          nev: "Romain",
-          osztaly: "13d",
-          szakkorId: null,
-        },
-        {
-          id: 12,
-          nev: "crypted",
-          osztaly: "13e",
-          szakkorId: null,
-        },
-        {
-          id: 13,
-          nev: "skulleewag",
-          osztaly: "13e",
-          szakkorId: null,
-        },
-        {
-          id: 14,
-          nev: "Kacper Jarzynski",
-          osztaly: "13e",
-          szakkorId: null,
-        },
-        {
-          id: 15,
-          nev: "huh_bokii",
-          osztaly: "13a",
-          szakkorId: null,
-        },
-        {
-          id: 16,
-          nev: "DimaNastart",
-          osztaly: "13b",
-          szakkorId: null,
-        },
+        { id: 1, nev: 'AVE', osztaly: '13d', szakkorId: 1 },
+        { id: 2, nev: 'Lexus_DC', osztaly: '13d', szakkorId: 4 },
+        { id: 3, nev: 'Farlix', osztaly: '13d', szakkorId: 2 },
+        { id: 4, nev: 'Amy', osztaly: '13d', szakkorId: 3 },
+        { id: 5, nev: 'ItzKat1', osztaly: '13d', szakkorId: null },
+        { id: 6, nev: 'surix', osztaly: '13d', szakkorId: null },
+        { id: 7, nev: 'Begi', osztaly: '13d', szakkorId: 1 },
+        { id: 8, nev: 'SanielShadowX', osztaly: '13a', szakkorId: null },
+        { id: 9, nev: 'Aaron Collins', osztaly: '13d', szakkorId: null },
+        { id: 10, nev: 'Patr1ck01', osztaly: '14d', szakkorId: null },
+        { id: 11, nev: 'Romain', osztaly: '13d', szakkorId: 2 },
+        { id: 12, nev: 'crypted', osztaly: '13e', szakkorId: null },
+        { id: 13, nev: 'skulleewag', osztaly: '13e', szakkorId: null },
+        { id: 14, nev: 'Kacper Jarzynski', osztaly: '13e', szakkorId: 3 },
+        { id: 15, nev: 'huh_bokii', osztaly: '13a', szakkorId: 4 },
+        { id: 16, nev: 'DimaNastart', osztaly: '13b', szakkorId: null },
+        { id: 17, nev: 'JackusEditz', osztaly: '13c', szakkorId: null },
+        { id: 18, nev: 'shizaseen', osztaly: '13d', szakkorId: 1 },
       ],
       szakkorok: [
-        {
-          id: 1,
-          nev: "Nem jár szakkörre",
-        },
-        {
-          id: 2,
-          nev: "Történelem",
-        },
-        {
-          id: 3,
-          nev: "Matematika",
-        },
-        {
-          id: 4,
-          nev: "Gamer",
-        },
+        { id: 1, nev: 'Nem jár szakkörre' },
+        { id: 2, nev: 'Történelem' },
+        { id: 3, nev: 'Matematika' },
+        { id: 4, nev: 'Gamer' },
       ],
     };
   },
 };
 </script>
-
-
 
 <style>
 .my-border {
